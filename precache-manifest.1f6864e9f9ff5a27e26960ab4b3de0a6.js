e9c68ce99dbafb5d86b5384d493959a4ed3d9b32
Pranav <pranav@geeykants.com>
2022-01-02 16:05:59 +0530

    Updates

---
+++ precache-manifest.1f6864e9f9ff5a27e26960ab4b3de0a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452d7e63f677b4ffb599866887748035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e38a34f50c4211214f61",
+    "url": "/static/css/main.5975c334.chunk.css"
+  },
+  {
+    "revision": "75f470a9709d67e6609a",
+    "url": "/static/js/2.30c4f410.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.30c4f410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38a34f50c4211214f61",
+    "url": "/static/js/main.c51d54fd.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pranav-pandey/precache-manifest.1dec55a46c310f9faf1b78c98b1d1719.js"
+  "/precache-manifest.1f6864e9f9ff5a27e26960ab4b3de0a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pranav-pandey/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
